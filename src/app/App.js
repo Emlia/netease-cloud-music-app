cd820cf92e9346a4a69956463ecb6801f1e0226c
emlia <771631347@qq.com>
2021-07-10 21:56:13 +0800

    init project

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "../pages/home";
+import Mine from "../pages/mine";
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+      <Mine></Mine>
+    </div>
+  );
+}
+
+export default App;
